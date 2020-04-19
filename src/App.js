870f7997921bace08869274562a94d9a4db5294a
z20240 <z20240z@gmail.com>
2020-04-20 02:34:02 +0800

    testing counter

--- src/App.js
+++ src/App.js
@@ -5,23 +5,19 @@ import './App.css';
 import { renderRoutes } from 'react-router-config';
 import { HashRouter } from 'react-router-dom';
 import router from './router';
-import { ThemeProvider } from 'styled-components';
-import theme from './theme';
 
 const loading = () => <div className="animated fadeIn pt-1 text-center">Loading...</div>;
 
 function App() {
     return (
-        <ThemeProvider theme={theme}>
-            <HashRouter>
-                {/** https://blog.csdn.net/roamingcode/article/details/95235079 */}
-                {/** child routes won't render without this */}
-                {/** 渲染匹配路徑的路有組件 */}
-                <Suspense fallback={loading()}>
-                    { renderRoutes(router) }
-                </Suspense>
-            </HashRouter>
-        </ThemeProvider>
+        <HashRouter>
+            {/** https://blog.csdn.net/roamingcode/article/details/95235079 */}
+            {/** child routes won't render without this */}
+            {/** 渲染匹配路徑的路有組件 */}
+            <Suspense fallback={loading()}>
+                { renderRoutes(router) }
+            </Suspense>
+        </HashRouter>
     );
 }
 
--- src/views/login/index.js
+++ src/views/login/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import StylInput from '../../components/styleComponents/stylInput';
-import StylButton from '../../components/styleComponents/stylButton';
 
 const Login = () => {
     return (
         <div>
-            <StylInput variant='danger' size='small' />
-            <StylButton>teste</StylButton>
+            <p>Test</p>
+            <input variant='danger' size='small' />
+            <button>tesjjjjjjjjjjte</button>
         </div>
     );
 };
